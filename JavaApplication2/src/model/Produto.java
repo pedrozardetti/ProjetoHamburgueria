2b716173a1a3642abcf763ec85df63f31caf8532
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-04-04 17:26:15 -0300

    Objetos do sistema (produto)

---
+++ JavaApplication2/src/model/Produto.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author pedro
+ */
+public class Produto {
+    private String nome;
+    private Double preco;
+    private String descricao;
+
+    public Produto() {
+    }
+    
+    
+    public Produto(String nome, Double preco, String descricao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+}
