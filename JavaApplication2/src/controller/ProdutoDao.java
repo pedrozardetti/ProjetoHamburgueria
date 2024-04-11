d0bd3972f3273096338abfa9142406fa6d1e3325
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-04-11 17:03:30 -0300

    Feito métodos atualizar e deletar na tela ver produtos

--- JavaApplication2/src/controller/ProdutoDao.java
+++ JavaApplication2/src/controller/ProdutoDao.java
@@ -5,6 +5,10 @@
 package controller;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JOptionPane;
 import model.Produto;
 
 /**
@@ -42,9 +46,99 @@ public class ProdutoDao extends ConectarDao {
             con.close();
 
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null,
+                    "Não foi possível realizar o cadastro", "Erro de Operação",
+                    JOptionPane.WARNING_MESSAGE);
 
         }
 
+    }
+
+    public List<Produto> listProduto() {
+        String sql = "SELECT * FROM PRODUTO";
+
+        try {
+            Connection con = Conectar();
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Produto> produtos = new ArrayList<>();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                double preco = resultSet.getDouble("preco");
+                String descricao = resultSet.getString("descricao");
+
+                produtos.add(new Produto(id, nome, preco, descricao));
+            }
+
+            System.out.println("Sucesso em selecionar e colocar na lista!");
+            return produtos;
+        } catch (Exception e) {
+            System.out.println("Erro em selecionar!");
+            return Collections.emptyList();
+        }
+    }
+
+    public Produto procurarProduto(Integer id) {
+        String sql = "SELECT * FROM PRODUTO WHERE ID = ?";
+
+        Produto produto = null;
+
+        try {
+            Connection con = Conectar();
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                produto = new Produto(resultSet.getInt("id"), resultSet.getString("nome"), resultSet.getDouble("preco"), resultSet.getString("descricao"));
+
+            }
+            return produto;
+
+        } catch (Exception e) {
+            System.out.println("Falhou :(");
+            return null;
+        }
+
+    }
+
+    public void atualizarProduto(Integer id, String nome, double preco, String descricao) {
+        String sql = "UPDATE PRODUTO SET NOME = ?, preco = ?, descricao = ? WHERE ID = ?";
+
+        try {
+            Connection con = Conectar();
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, nome);
+            preparedStatement.setDouble(2, preco);
+            preparedStatement.setString(3, descricao);
+            preparedStatement.setInt(4, id);
+            preparedStatement.execute();
+
+            System.out.println("Update feito com sucesso!");
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Update não foi realizado!");
+            return;
+        }
+
+    }
+    
+    public void deletarProduto(Integer id) {
+        String sql = "DELETE FROM PRODUTO WHERE ID = ?";
+        
+        try {
+            Connection con = Conectar();
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.execute(); 
+            
+            System.out.println("Produto foi deletado com sucesso!");
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Não foi possível deletar o produto!");
+        }
+        
     }
 }
