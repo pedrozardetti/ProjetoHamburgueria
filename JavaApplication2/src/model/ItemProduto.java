43c4150fd7526db38f74b1a35632cc44f752bb2b
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-05-22 17:39:45 -0300

    feat construtores, getters e setters

--- JavaApplication2/src/model/ItemProduto.java
+++ JavaApplication2/src/model/ItemProduto.java
@@ -9,5 +9,55 @@ package model;
  * @author pedro
  */
 public class ItemProduto {
-    
+
+    private int id;
+    private String nome;
+    private double preco;
+    private int id_produto;
+
+    public ItemProduto(String nome, double preco, int id_produto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.id_produto = id_produto;
+    }
+
+    public ItemProduto(int id, String nome, double preco, int id_produto) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.id_produto = id_produto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
 }
