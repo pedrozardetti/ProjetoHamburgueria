0607a6e2a703b5aaa4aab56b8a9c61db66a32f1c
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-05-22 17:39:09 -0300

    feat PedidoDao

--- JavaApplication2/src/controller/PedidoDao.java
+++ JavaApplication2/src/controller/PedidoDao.java
@@ -3,11 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controller;
-
+import java.sql.*;
 /**
  *
  * @author pedro
  */
-public class PedidoDao extends ConectarDao{
+public class PedidoDao extends ConectarDao {
+
     
 }
