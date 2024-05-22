c78bc5bb9a7fe21bf25436d53bc6af71452da7ff
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-05-22 17:40:24 -0300

    feat construtores, getters e setters

--- JavaApplication2/src/model/Pedido.java
+++ JavaApplication2/src/model/Pedido.java
@@ -4,10 +4,71 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import model.enums.TipoPedido;
+
 /**
  *
  * @author pedro
  */
 public class Pedido {
+
+    private int id;
+    private double total;
+    private TipoPedido status;
+    private List<ItemProduto> produtos;
+
+    public Pedido(int id, TipoPedido status) {
+        this.id = id;
+        this.total = getTotal();
+        this.status = status;
+        this.produtos = new ArrayList<>();
+    }
+
+    public Pedido(double total, TipoPedido status) {
+        this.total = total;
+        this.status = status;
+        this.produtos = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTotal() {
+    return produtos.stream()
+                   .mapToDouble(ItemProduto::getPreco)
+                   .sum();
+}
+
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public TipoPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(TipoPedido status) {
+        this.status = status;
+    }
+
+    public List<ItemProduto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<ItemProduto> produtos) {
+        this.produtos = produtos;
+    }
+
+   
+    
     
 }
